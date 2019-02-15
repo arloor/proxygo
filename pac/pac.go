4351a7fdd71fec6b2b1dbf3c52ea5e7a2f716f16
arloor <admin@arloor.com>
2019-02-15 09:32:45 +0800

    考虑到跨平台，默认不设置注册表

--- pac/pac.go
+++ pac/pac.go
@@ -2,7 +2,6 @@ package pac
 
 import (
 	"fmt"
-	"golang.org/x/sys/windows/registry"
 	"io/ioutil"
 	"net/http"
 	"runtime"
@@ -11,6 +10,8 @@ import (
 var buf, err = ioutil.ReadFile("pac.txt")
 
 const pacUrl = "http://127.0.0.1:9999/pac"
+
+var setWindowsRegistry func()
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(buf))
@@ -40,27 +41,4 @@ func ServePAC() {
 	if err := http.ListenAndServe("127.0.0.1:9999", nil); err != nil {
 		fmt.Println("serve PAC过程中出错")
 	}
-}
-
-func setWindowsRegistry() {
-	//设置注册表
-	key, exists, err := registry.CreateKey(registry.CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", registry.ALL_ACCESS)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer key.Close()
-
-	if exists {
-		//fmt.Println("键已存在")
-	} else {
-		fmt.Println("此注册表项不存在，已自动新建")
-	}
-	// 写入字符串
-	key.SetStringValue("AutoConfigURL", pacUrl)
-	key.SetStringValue("ProxyServer", "127.0.0.1:8081")
-	key.SetStringValue("ProxyOverride", "localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;172.32.*;192.168.*;127.0.0.1;<local>")
-	// 写入32位整形值
-	key.SetDWordValue("MigrateProxy", 0x00000001)
-	key.SetDWordValue("ProxyEnable", 0x00000000)
-	fmt.Println("设置windows代理相关注册表成功")
 }
---
+++ pac/windows.go
@@ -0,0 +1,31 @@
+// +build windows
+
+package pac
+
+import (
+	"fmt"
+	"golang.org/x/sys/windows/registry"
+)
+
+func setWindowsRegistry() {
+	//设置注册表
+	key, exists, err := registry.CreateKey(registry.CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", registry.ALL_ACCESS)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer key.Close()
+
+	if exists {
+		//fmt.Println("键已存在")
+	} else {
+		fmt.Println("此注册表项不存在，已自动新建")
+	}
+	// 写入字符串
+	key.SetStringValue("AutoConfigURL", pacUrl)
+	key.SetStringValue("ProxyServer", "127.0.0.1:8081")
+	key.SetStringValue("ProxyOverride", "localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;172.32.*;192.168.*;127.0.0.1;<local>")
+	// 写入32位整形值
+	key.SetDWordValue("MigrateProxy", 0x00000001)
+	key.SetDWordValue("ProxyEnable", 0x00000000)
+	fmt.Println("设置windows代理相关注册表成功")
+}
