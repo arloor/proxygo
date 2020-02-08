e902704e41a5081e9434f2c5b3db92d4f51387ff
arloor <admin@arloor.com>
2020-02-08 14:50:04 +0800

    修改编译命令

--- 编译命令.bat
+++ 编译命令.bat
@@ -1,4 +1,10 @@
 SET CGO_ENABLED=0
+SET GOOS=windows
+SET GOARCH=amd64
+go build -o build/proxygo_dev.exe
+go build -ldflags="-H windowsgui" -o build/proxygo.exe
+
+SET CGO_ENABLED=0
 SET GOOS=darwin
 SET GOARCH=amd64
 go build -o build/proxygo_mac
@@ -7,7 +13,3 @@ SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
 go build -o build/proxygo
-
-go build -o build/proxygo_dev.exe
-
-go build -ldflags="-H windowsgui" -o build/proxygo.exe
